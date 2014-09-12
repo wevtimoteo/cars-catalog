31551516532c62be21f945b74ff70d397be4e23b
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2014-09-12 09:07:29 -0300

    Implement CCCar model class

---
+++ Cars-Catalog/CCCar.h
@@ -0,0 +1,23 @@
+//
+//  CCCar.h
+//  Cars-Catalog
+//
+//  Created by Weverton Couto Timoteo on 9/12/14.
+//  Copyright (c) 2014 Weverton Couto Timoteo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCCar : NSObject
+
+@property (strong, nonatomic) NSString *objectId;
+@property (strong, nonatomic) NSString *modelName;
+@property (assign, nonatomic) NSInteger year;
+@property (assign, nonatomic) NSInteger traveledKilometers;
+@property (strong, nonatomic) NSString *manufacturer;
+@property (strong, nonatomic) NSDate *createdAt;
+@property (strong, nonatomic) NSDate *updatedAt;
+
+- (id)initFromDictionary:(NSDictionary *)data;
+
+@end
