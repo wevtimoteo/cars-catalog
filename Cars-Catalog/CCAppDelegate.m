6742e7870ce93dc2dc9bcd8340e6a281c76bc6ab
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2014-09-13 10:11:36 -0300

    Removes UIStoryboard bootstrap references

--- Cars-Catalog/CCAppDelegate.m
+++ Cars-Catalog/CCAppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self.window makeKeyAndVisible];
+
     [self logNetworkActivity];
     [self displayNavigation];
 
