b69b18b4a7a35d7ad26230c5ba83d211db500625
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2014-09-14 17:20:07 -0300

    Create CCCarRequest class

---
+++ Cars-Catalog/CCCarRequest.h
@@ -0,0 +1,13 @@
+//
+//  CCCarRequest.h
+//  Cars-Catalog
+//
+//  Created by Weverton Couto Timoteo on 9/14/14.
+//  Copyright (c) 2014 Weverton Couto Timoteo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCCarRequest : NSObject
+
+@end
